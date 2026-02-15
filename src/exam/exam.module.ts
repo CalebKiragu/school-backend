ba9c5c94dbfecd6f3ed4c0c9230fede33e5bd0c1
Caleb Kiragu <calebdavidbusiness@gmail.com>
2026-02-15 19:37:58 +0300

    Fix ExamModule - add ExamResult entity to TypeORM imports

--- src/exam/exam.module.ts
+++ src/exam/exam.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ExamService } from './exam.service';
 import { ExamResultsUssdView } from './entities/exam-results-ussd.view';
+import { ExamResult } from './entities/exam-result.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ExamResultsUssdView])],
+  imports: [TypeOrmModule.forFeature([ExamResultsUssdView, ExamResult])],
   providers: [ExamService],
   exports: [ExamService],
 })
